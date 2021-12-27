f8ebdf21fd73ef0287091603cb97f62683918330
Noel Kocheril <NoelKocheril@Gmail.com>
2021-12-27 12:38:42 -0500

    Added Solution for Day 02

--- Solutions/day02.py
+++ Solutions/day02.py
@@ -2,8 +2,43 @@ from utils.solutionBase import SolutionBase
 
 
 class Solution(SolutionBase):
-    def part_01(self, data: list[int]) -> int:
-        return -1
+    def part_01(self, data: list[str]) -> int:
+        horPos, vertPos = 0, 0  # initial horizontal and vertical position to 0
+
+        for entry in data:
+            movement, amount = entry.split()
+            amount = int(amount)
+
+            if movement == "forward":
+                horPos += amount
+            elif movement == "up":
+                vertPos -= amount
+            elif movement == "down":
+                vertPos += amount
+            else:
+                raise ValueError("Invalid movement")
+
+            # print(f"We are moving {movement} by {amount} amount")
+
+        return horPos * vertPos
 
     def part_02(self, data: list[int], slidingWindowSize: int = 3) -> int:
-        return -1
+        horPos, vertPos, aim = 0, 0, 0  # initial horizontal and vertical position to 0
+
+        for entry in data:
+            movement, amount = entry.split()
+            amount = int(amount)
+
+            if movement == "forward":
+                horPos += amount
+                vertPos += aim * amount
+            elif movement == "up":
+                aim -= amount
+            elif movement == "down":
+                aim += amount
+            else:
+                raise ValueError("Invalid movement")
+
+            # print(f"We are moving {movement} by {amount} amount")
+
+        return horPos * vertPos
